4d49a9c1a5746fd0e19f1f4767619e0b18ae287c
luisfeob34 <luis.vasconcellos@sou.unifeob.edu.br>
2026-05-07 21:39:47 -0300

    Delete Entrega-07-05 directory

--- Entrega-07-05/app.py
+++
@@ -1,6 +0,0 @@
-from controllers.controller import CinemaController
-
-if __name__ == "__main__":
-    # O Controller orquestra o início da aplicação
-    sistema = CinemaController()
-    sistema.iniciar()
